6353123659f5237abf8ae203418f7e01a002391d
surajmh <93497828+surajmh@users.noreply.github.com>
2022-07-12 01:18:16 +1000

    init

---
+++ src/Technologies.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.scss";
+
+const Technologies = () => (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+        <div>Stack profile</div>
+        <div>Framework: react</div>
+        <div>Language: TypeScript</div>
+        <div>CSS: Tailwind</div>
+    </div>
+);
+
+export { Technologies }
